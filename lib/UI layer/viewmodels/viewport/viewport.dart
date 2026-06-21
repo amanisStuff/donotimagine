1c6e1f41a4f31865225692327d29d9377ea014a9
amani kherraz <amanikherraz@gmail.com>
2026-06-21 22:13:22 +0100

    add borders  and selection to gallery

--- lib/UI layer/viewmodels/viewport/viewport.dart
+++ lib/UI layer/viewmodels/viewport/viewport.dart
@@ -38,13 +38,22 @@ class ImageViewport extends ChangeNotifier implements AppViewport {
   @override
   Widget setUpUI() {
     // TODO: implement setUpUI
-    return gellery();
+    return ImageGellery();
   }
 }
 
-class gellery extends StatelessWidget {
-  const gellery({super.key});
-
+class ImageGellery extends StatelessWidget {
+  const ImageGellery({
+    super.key,
+    this.imagePaths = const [],
+    this.onAddImageClick,
+    this.onImageClick,
+    this.selectedIndex = 2,
+  });
+  final List<String> imagePaths;
+  final GestureTapCallback? onAddImageClick;
+  final Function(int)? onImageClick;
+  final int? selectedIndex;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -56,26 +65,48 @@ class gellery extends StatelessWidget {
           mainAxisSpacing: 8,
         ),
         children: List.generate(
-          15,
+          imagePaths.length + 1,
           (index) => Container(
             decoration: BoxDecoration(
+              color: selectedIndex == index ? Colors.red : Colors.white,
               borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
-            clipBehavior: Clip.antiAlias,
-            child: index == 14
-                ? InkWell(
-                    onTap: () {
-                      print('Container Clicked via InkWell!');
-                    },
-                    child: MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: Container(
-                        color: Colors.amber,
-                        child: Icon(Icons.image_search, size: 32),
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: index == imagePaths.length
+                    ? InkWell(
+                        onTap: onAddImageClick,
+                        child: MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: Container(
+                            color: Colors.amber,
+                            child: Icon(Icons.image_search, size: 32),
+                          ),
+                        ),
+                      )
+                    : InkWell(
+                        onTap: () {
+                          onImageClick == null
+                              ? "do nothing"
+                              : onImageClick!(index);
+                        },
+                        child: MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child:
+                              Image.network(
+                                    imagePaths[index],
+                                    fit: BoxFit.cover,
+                                  )
+                                  as Widget,
+                        ),
                       ),
-                    ),
-                  )
-                : Image.network("https://picsum.photos/200") as Widget,
+              ),
+            ),
           ),
         ),
       ),
