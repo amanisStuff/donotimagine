af6d3be4c363d1f4fccd677672c0decd95d11eeb
amani kherraz <amanikherraz@gmail.com>
2026-06-21 22:22:26 +0100

    add image display

--- lib/UI layer/viewmodels/viewport/viewport.dart
+++ lib/UI layer/viewmodels/viewport/viewport.dart
@@ -37,8 +37,20 @@ class ImageViewport extends ChangeNotifier implements AppViewport {
 
   @override
   Widget setUpUI() {
+    bool displayImage = true;
     // TODO: implement setUpUI
-    return ImageGellery();
+    return displayImage ? Imagedisplay() : ImageGellery();
+  }
+}
+
+class Imagedisplay extends StatelessWidget {
+  const Imagedisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      "https://64.media.tumblr.com/9ef5ecc820d8339eccf9c464da03478d/0ad2a6e9210cc2de-fa/s2048x3072/7d92f7045a35bc2c328b60e4d5bc39c9d193f9ce.pnj",
+    );
   }
 }
 
