751c7b6d41ed162847b7f0bf82c67f4b14d9e1b4
=amani kherraz <=amanikherraz@gmail.com>
2026-06-28 22:17:16 +0100

    seperate gallery into it's own class

--- src/main/java/com/mycompany/donotimagine/viewport/imageviewport/ImageViewport.java
+++ src/main/java/com/mycompany/donotimagine/viewport/imageviewport/ImageViewport.java
@@ -28,8 +28,7 @@ public class ImageViewport extends JPanel{
     JPanel imageView;
 
     public ImageViewport() {
-        gallery = new JPanel();
-        renderGallery();
+        gallery = new gallery(images, currentImageIndex);
         imageView = new JPanel();
         JButton nextButton = new JButton("next!!!");
         nextButton.addActionListener(e->{
@@ -42,30 +41,46 @@ public class ImageViewport extends JPanel{
     }
     public void setCurrentImage(int index){
         currentImageIndex = index % images.length;
-        renderGallery();
+        
     }
-    private void renderGallery(){
-        gallery.removeAll();
-        gallery.setLayout(new FlowLayout(FlowLayout.LEFT));
-        for (int i = 0; i < images.length; i++) {
-            ImageContainer imageContainer = new ImageContainer();
-            //imageContainer.setBackground(Color.blue);
-            imageContainer.setPreferredSize(new Dimension(100,100));
-            JLabel imagelabel = new JLabel("this is image number " + images[i]);
-            if (i == currentImageIndex) {
-                imagelabel.setForeground(Color.red);
-            }
-            imageContainer.add(imagelabel);
-            gallery.add(imageContainer);
-        }
-        gallery.revalidate();
-        gallery.repaint();
-    }
+    
     
 }
 class ImageContainer extends JPanel{
 
     public ImageContainer() {
         this.setBorder(BorderFactory.createLineBorder(Color.yellow));
+    }
+}
+class gallery extends JPanel{
+    int focused_image;
+    String[] images= {"1","2","3","4","5"};
+
+    public gallery() {
+    }
+    public gallery(String[] images ,int focused_image) {
+        this.images=images;
+        this.focused_image =focused_image;
+        renderGallery();
+    }
+    public void setfocus(int index){
+        focused_image = index;
+        renderGallery();
+    }
+    private void renderGallery(){
+        this.removeAll();
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        for (int i = 0; i < images.length; i++) {
+            ImageContainer imageContainer = new ImageContainer();
+            imageContainer.setPreferredSize(new Dimension(100,100));
+            JLabel imagelabel = new JLabel("this is image number " + images[i]);
+            if (i == focused_image) {
+                imagelabel.setForeground(Color.red);
+            }
+            imageContainer.add(imagelabel);
+            this.add(imageContainer);
+        }
+        this.revalidate();
+        this.repaint();
     }
 }
