b9bf7df6d416c5ca490667ee4099993ec52f3cbf
amani kherraz <amanikherraz@gmail.com>
2026-06-29 16:13:11 +0100

    io

--- src/main/java/com/mycompany/donotimagine/IOHandeling.java
+++ src/main/java/com/mycompany/donotimagine/IOHandeling.java
@@ -4,14 +4,48 @@
  */
 package com.mycompany.donotimagine;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import org.xml.sax.InputSource;
+
 /**
  *
  * @author amani
  */
 public class IOHandeling {
-    static public Object loadFile(){
-        return null;
+
+    public static enum imageOptions {
+        WEB,
+        SYSTEM,
+        ASSET
+    };
+
+    static Image loadImage(String source, imageOptions imageOption) throws IOException, AssertionError {
+        BufferedImage loaded;
+        switch (imageOption) {
+            case ASSET:
+                InputStream is = Image.class.getResourceAsStream("/images/logo.png");
+                BufferedImage imgFromResource = ImageIO.read(is);
+                break;
+            case SYSTEM:
+                loaded = ImageIO.read(new File(source));
+                break;
+            case WEB:
+                loaded = ImageIO.read(new URL("https://example.com"));
+                break;
+            default:
+                throw new AssertionError();
+        }
+        return ImageIO.read(new File(source));
     }
-    static public void saveFile(){}
-    
+
+    static public void saveImage(Image image) {
+
+    }
+
 }
