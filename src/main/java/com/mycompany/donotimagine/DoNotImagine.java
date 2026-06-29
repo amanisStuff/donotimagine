2b475c7508bfe0b463b412388ce0fd19cd8ab889
amani kherraz <amanikherraz@gmail.com>
2026-06-29 13:48:49 +0100

    redo

--- src/main/java/com/mycompany/donotimagine/DoNotImagine.java
+++ src/main/java/com/mycompany/donotimagine/DoNotImagine.java
@@ -3,21 +3,8 @@
  */
 
 package com.mycompany.donotimagine;
-import com.mycompany.donotimagine.viewport.imageviewport.ImageViewportProvider;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javax.swing.JFrame;
-
 public class DoNotImagine {
-
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        ImageViewportProvider imv = new ImageViewportProvider();
-        frame.setTitle("frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(250,250));
-        frame.setLayout(new BorderLayout());
-        frame.add(imv.getViewport());
-        frame.setVisible(true);
+        
     }
 }
